e5a9231f825132c16a10830ef90f3011b7547a7b
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-15 17:49:16 -0500

    test on App and tests on Episodes complete

---
+++ src/App.test.js
@@ -0,0 +1,33 @@
+//import deps etc
+import React from "react";
+import { render, fireEvent, waitFor, act } from "@testing-library/react";
+
+//Do I need to import any other items from react testing? fireEvnt, waitfor, act??
+//import and create mock
+import { fetchShow as mockFetchShow } from "./api/fetchShow";
+//import App
+import App from "./App";
+
+//create jest mock
+jest.mock("./api/fetchShow");
+
+//the test
+//mock data
+//arrange, act, assert
+//which method ??
+
+test("App fetches show data and renders it", async () => {
+  const mockData = {
+    id: "123",
+    image: { medium: "medium_image" },
+    name: "name",
+    season: 3,
+    number: 2,
+    summary: "<p>Summary</p>",
+    runtime: 20,
+  };
+  mockFetchShow.mockResolvedValueOnce(mockData);
+  const { queryAllByText } = render(<App />);
+  // now wait
+  await waitFor(() => expect(queryAllByText(/name/i)).toHaveLength(1));
+});
